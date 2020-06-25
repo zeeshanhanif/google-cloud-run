6cc7dc53db137e6a014b196ecdc5ddcd181a6b9e
zeeshanhanif <zeeshanhanif@gmail.com>
2020-06-25 14:29:42 +0500

    updated react client app

--- react-cloud-app/src/App.js
+++ react-cloud-app/src/App.js
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-
+  console.log("NODE_ENV = ", process.env.NODE_ENV);
+  console.log("Before API_SERVER_URL = ",process.env.API_SERVER_URL);
   const [isFetching, setFetching] = useState(false);
   const [user, setUser] = useState({});
-
+  const API_SERVER_URL = process.env.API_SERVER_URL || "https://helloworld-lkewgry7ca-de.a.run.app"
+  console.log("After API_SERVER_URL = ",API_SERVER_URL);
   useEffect(()=> {
     (async ()=>{
       setFetching(true);
-      const response = await fetch("https://helloworld-lkewgry7ca-de.a.run.app/user");
+      const response = await fetch(API_SERVER_URL+"/user");
       const userData = await response.json();
       console.log(userData);
       setUser(userData)
@@ -18,11 +20,13 @@ function App() {
   },[0]);
 
   if(isFetching){
+    
     return <div>Data Loading.....</div>
   }
   return (
+    
     <div>
-      Hello World from Cloud Run
+      Hello World updated from Cloud Run
       <div>Name: {user.name}</div>
       <div>Age: {user.age}</div>
     </div>
