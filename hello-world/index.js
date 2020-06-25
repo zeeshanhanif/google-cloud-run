df9ee8232d54a7feb7b5bbfabb0c496b00c301bf
zeeshanhanif <zeeshanhanif@gmail.com>
2020-06-25 13:35:51 +0500

    added server api

---
+++ hello-world/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+// Add headers
+app.use(function (req, res, next) {
+  var origin = req.headers.origin;
+  res.setHeader('Access-Control-Allow-Origin', origin || "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+
+
+app.get('/', (req, res) => {
+  console.log('Hello world received a request.');
+
+  const target = process.env.TARGET || 'World';
+  res.send(`Hello ${target}!`);
+});
+
+app.get('/user', (req, res) => {
+  console.log('/user received a request.');
+
+  res.json({name: "John", age: 45});
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Hello world listening on port', port);
+});
